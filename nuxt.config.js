eae02552ea2152746a5a5b2ee7f0d47598d2dd6f
dalvarez082 <diego980910@gmail.com>
2023-03-28 19:26:47 -0500

    Limpiando entorno de trabajo

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,24 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Api_Marvel',
-    title: 'Api_Marvel',
+    title: 'Api_Marvel',    
     htmlAttrs: {
       lang: 'en',
-    },
+    },   
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }], 
+
+
   },
+
+  
+
+ 
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -54,15 +60,6 @@ export default {
       
     
   },
-
-  text:"hola",
-
-
-
-
-
-
-
 
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,84 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</template>
+    <div class="container">
+      <h1>Personajes de Marvel</h1>
+      <div class="characters">
+        <div v-for="character in characters" :key="character.id" class="character">
+          <div class="overlay"></div>
+          <img :src="character.thumbnail.path + '/portrait_fantastic.' + character.thumbnail.extension" :alt="'Imagen de ' + character.name" />
+          <h2>{{ character.name }}</h2>
+        </div>
+      </div>
+    </div>
+  </template>
+  <style>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+  
+  .characters {
+    display: flex;
+    overflow-x: scroll;
+    scroll-behavior: smooth;
+  }
+  
+  .character {
+    position: relative;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+  
+  .character img {
+    width: 200px;
+    height: 300px;
+    object-fit: cover;
+  }
+  
+  .character .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+  
+  .character:hover .overlay {
+    opacity: 1;
+  }
+  
+  .character h2 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 10px;
+    margin: 0;
+    font-size: 16px;
+    color: #fff;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
+  }
+  </style>
+  
+  <script>
 
-<script>
-export default {
-  name: 'IndexPage',
-}
-</script>
+  
+  export default {
+    data() {
+      return {
+        characters: []
+      }
+    },
+    async mounted() {
+        console.log(this)
+      const response = await this.$axios.$get('characters',{params:{
+        apikey: 'ad6bdd113a25813549da8c25b33f1997'}})
+      this.characters = response.data.results
+      console.log(this.characters)
+    }
+  }
+  </script>
+  
+  
