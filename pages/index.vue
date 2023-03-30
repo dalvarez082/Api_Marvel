b50c5556ce393c84c608b80d380eab3bf21c9f1e
dalvarez082 <diego980910@gmail.com>
2023-03-30 07:39:16 -0500

    Actualización de diseño

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,107 @@
 <template>
-    <div class="container">
-      <h1>Personajes de Marvel</h1>
-      <div class="characters">
-        <div v-for="character in characters" :key="character.id" class="character">
-          <div class="overlay"></div>
-          <img :src="character.thumbnail.path + '/portrait_fantastic.' + character.thumbnail.extension" :alt="'Imagen de ' + character.name" />
-          <h2>{{ character.name }}</h2>
-        </div>
-      </div>
+  <div class="container">
+    <div class="segment-one">   
+      <h2>Personajes de marvel</h2>   
+      <v-slide-group>
+        <v-slide-item v-for="(character, index) in characters" :key="index">
+          <div class="character-card">
+            <img :src="character.thumbnail.path + '/portrait_fantastic.' + character.thumbnail.extension" alt="Character image">
+            <p>{{ character.name }}</p>
+          </div>
+        </v-slide-item>
+      </v-slide-group>
     </div>
-  </template>
-  <style>
-  .container {
-    max-width: 1000px;
-    margin: 0 auto;
-    padding: 20px;
-  }
-  
-  .characters {
-    display: flex;
-    overflow-x: scroll;
-    scroll-behavior: smooth;
-  }
-  
-  .character {
-    position: relative;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-  
-  .character img {
-    width: 200px;
-    height: 300px;
-    object-fit: cover;
-  }
-  
-  .character .overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
+    <div class="segment-two">
+      <h2>Segment Two</h2>
+    </div>
+    <div class="segment-three">
+      <h2>Segment Three</h2>
+    </div>
+  </div>
+</template>
+
+<style>
+
+html,
+  body {
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out;
-  }
-  
-  .character:hover .overlay {
-    opacity: 1;
-  }
-  
-  .character h2 {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    padding: 10px;
     margin: 0;
-    font-size: 16px;
-    color: #fff;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);
+    padding: 0;
   }
-  </style>
-  
-  <script>
+
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    height: 100%;
+    width: 100%;
+    max-width: 100%;
+    box-sizing: border-box;
+   
+  }
+
+  .segment-one {
+    
+    background-color: #b82626;
+    height: 50%;    
+    align-items: flex-start;
+    overflow: hidden;
+    
+  }
+  .segment-one h2 {
+    text-align: center; /* Centra horizontalmente el título */
+  }
+
+  .segment-two {
+    flex: 1 1 50%;
+    background-color: #15cf78;
+    height: 50%;
+  }
+
+  .segment-three {
+    flex: 1 1 50%;
+    background-color: #b716c3;
+    height: 50%;
+  }
 
   
+
+
+
+  
+  .v-slide-group {
+    display: flex;
+    justify-content: center;
+    margin-top: 1%;
+    
+    
+    width: 100%; /* Ajusta el ancho del componente al 80% del ancho de su contenedor padre */
+  }
+
+.character-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    padding: 20px;
+    text-align: center;
+
+}
+
+.character-card img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+
+
+
+
+
+</style>
+
+<script>
   export default {
     data() {
       return {
@@ -72,13 +109,15 @@
       }
     },
     async mounted() {
-        console.log(this)
-      const response = await this.$axios.$get('characters',{params:{
-        apikey: 'ad6bdd113a25813549da8c25b33f1997'}})
+      const response = await this.$axios.$get('characters', { params: { apikey: 'ad6bdd113a25813549da8c25b33f1997' }})
       this.characters = response.data.results
-      console.log(this.characters)
+      console.log(response)
     }
-  }
-  </script>
+  }
+</script>
+
+
+
+
   
   
